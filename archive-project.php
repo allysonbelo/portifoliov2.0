9134b4ada625b30cad73cc718104ee8def136ebe
allysonbelo <allysonbelo@gmail.com>
2026-08-10 09:27:16 -0300

    fix: add archive-project.php to resolve CPT archive routing for /en/project/

---
+++ archive-project.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Post Type Archive Template for Projects CPT
+ * 
+ * Automatically loads template-projetos.php for any CPT archive query (e.g. /en/project/).
+ */
+
+include get_template_directory() . '/template-projetos.php';
