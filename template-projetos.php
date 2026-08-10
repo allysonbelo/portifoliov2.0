19aefcba82f906cc1494e0c12c034b0d7b9581d6
allysonbelo <allysonbelo@gmail.com>
2026-08-10 10:30:04 -0300

    feat: use native page content in portfolio hero description with fallback to hero_desc

--- template-projetos.php
+++ template-projetos.php
@@ -31,9 +31,15 @@ $hero_desc    = get_field('portfolio_description') ?: abc_tech_tr('Uma seleção
                 <?php echo esc_html($hero_title_1); ?>
                 <span class="text-blue"><?php echo esc_html($hero_title_2); ?></span>
             </h1>
-            <p class="page-description">
-                <?php echo esc_html($hero_desc); ?>
-            </p>
+            <?php if (get_the_content()) : ?>
+                <div class="page-description">
+                    <?php the_content(); ?>
+                </div>
+            <?php else : ?>
+                <p class="page-description">
+                    <?php echo esc_html($hero_desc); ?>
+                </p>
+            <?php endif; ?>
         </div>
     </section>
 
